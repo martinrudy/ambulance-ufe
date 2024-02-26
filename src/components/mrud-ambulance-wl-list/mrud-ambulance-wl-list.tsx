2e6a705c36a32d05f6bebbfdabb515b6bde7c9f1
Martin  Rudolf <rudolfmato@gmail.com>
2024-02-26 15:27:43 +0100

    test update

--- src/components/mrud-ambulance-wl-list/mrud-ambulance-wl-list.tsx
+++ src/components/mrud-ambulance-wl-list/mrud-ambulance-wl-list.tsx
@@ -26,7 +26,7 @@ export class MrudAmbulanceWlList {
           estimatedDurationMinutes: 20,
           condition: 'Teploty'
       }, {
-          name: 'Ing. Ferdinand Trety',
+          name: 'Ing. Ferdinand dadasdasdtest',
           patientId: '10028',
           since: new Date(Date.now() - 72 * 60).toISOString(),
           estimatedStart: new Date(Date.now() + 5 * 60).toISOString(),
